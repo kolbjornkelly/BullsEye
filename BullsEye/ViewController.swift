3a359d726290fd74ba459e9f348e02d0458bda6c
Kolbjorn Kelly <kolbjorn.kelly@gmail.com>
2020-10-01 17:41:17 +0200

    first

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -7,19 +7,68 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var numLabel: UILabel!
+    @IBOutlet weak var numSlider: UISlider!
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var modeSwitch: UISwitch!
+    @IBOutlet weak var playAgainButton: UIButton!
+    
+    var randomNum = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        randomNum = Int(arc4random_uniform(101))
+        numLabel.text = "Move the slider to: \(randomNum)"
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func checkVal(_ sender: Any) {
+        
+        if (modeSwitch.isOn == false) {
+            
+            if (Int(numSlider.value) >= randomNum - 3 && Int(numSlider.value) <= randomNum + 3) {
+                
+                resultLabel.text = "Bullseye!"
+                resultLabel.backgroundColor = UIColor.green
+            }
+                
+            else {
+                resultLabel.text = "Woops, looks like you missed. You hit \(round(numSlider.value))..."
+                resultLabel.backgroundColor = UIColor.red
+            }
+        }
+        
+        else {
+            if (randomNum == Int(numSlider.value)) {
+                resultLabel.text = "Bullseye!"
+                resultLabel.backgroundColor = UIColor.green
+            }
+                
+            else {
+                resultLabel.text = "Woops, looks like you missed. You hit \(round(numSlider.value))..."
+                resultLabel.backgroundColor = UIColor.red
+            }
+        }
+        resultLabel.isHidden = false
+        playAgainButton.isHidden = false
+    }
+    
+    @IBAction func playAgainButton(_ sender: Any) {
+        numSlider.setValue(50.0, animated: false)
+        randomNum = Int(arc4random_uniform(101))
+        numLabel.text = "Move the slider to: \(randomNum)"
+        resultLabel.isHidden = true
+        playAgainButton.isHidden = true
+    }
 }
 
